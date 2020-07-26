cfd95b6a8689ba10c39f1a9c2ea60ed1dc4e7487
Farzin Mohammadi <farzinlize@live.com>
2020-07-26 21:16:09 +0430

    implemetn CNT command

--- prj.py
+++ prj.py
@@ -1,3 +1,5 @@
+from TrieFind import TrieNode
+
 from getopt import getopt
 import sys
 
@@ -48,6 +50,38 @@ def generates_blocks(sequence, block_size, overlap, fasta_filename='blocks.fasta
     fasta.close
 
 
+def read_blocks(filename):
+    blocks = []
+
+    with open(filename, 'r') as f:
+        for _ in range(7):
+            f.readline()
+
+        for line in f:
+            if '>' not in line:
+                blocks += [line]
+    
+    return blocks
+
+
+def count_kmer(blocks, k):
+    tree = TrieNode()
+    for block in blocks:
+        kmer_start = 0
+        kmer_end = k
+        while kmer_end < len(block):
+            kmer = block[kmer_start:kmer_end]
+            tree.add_frame(kmer)
+            kmer_start += 1
+            kmer_end +=1
+    return tree
+
+
+def report_count(tree, fasta_filename='kmers.fasta'):
+    with open(fasta_filename, 'w') as fasta:
+        fasta.write(tree.report(tree.max_min_count()))
+    
+
 ########################################
 #            main call                 #
 ########################################
@@ -56,11 +90,12 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         raise Exception('request command must be specified (read the description for supported commands)')
 
-    shortopt = 'b:o:f:s:c:'
-    longopts = ['block-size=', 'overlap=', 'filename=', 'sequence-index=', 'category=']
+    shortopt = 'b:o:f:s:c:k:t:'
+    longopts = ['block-size=', 'overlap=', 'filename=', 'sequence-index=', 'category=', 'block-target=']
 
     # default arguments
-    arg_dics = {'block-size':100 , 'overlap':0, 'filename':'default.out', 'sequence-index':0, 'category':'FLU'}
+    arg_dics = {'block-size':100 , 'overlap':0, 'filename':'default.out', 'k':5,
+            'sequence-index':0, 'category':'FLU', 'block-target':'default.out'}
 
     command = sys.argv[1]
 
@@ -75,7 +110,14 @@ if __name__ == "__main__":
         elif o in ['-s', '--sequence-index']:
             arg_dics.update({'sequence-index':int(a)})
         elif o in ['-c', '--category']:
-            arg_dics.update({'filename':a})
+            arg_dics.update({'category':a})
+        elif o in ['-t', '--block-target']:
+            arg_dics.update({'block-target':a})
+        elif o == '-k':
+            arg_dics.update({'k':int(a)})
     
     if command == 'BLK':
         generates_blocks(read_sequence(arg_dics['category'], arg_dics['sequence-index']), arg_dics['block-size'], arg_dics['overlap'], arg_dics['filename'])
+    elif command == 'CNT':
+        tree = count_kmer(read_blocks(arg_dics['block-target']), arg_dics['k'])
+        report_count(tree, arg_dics['filename'])
